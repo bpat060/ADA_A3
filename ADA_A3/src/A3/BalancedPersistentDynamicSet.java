5fedbf574f567ddff027f28fc1e2a7375dc2802a
Bhakti Patel <87521790+bpat060@users.noreply.github.com>
2022-09-29 13:27:20 +1300

    balanced persistent dynamic set

---
+++ ADA_A3/src/A3/BalancedPersistentDynamicSet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A3;
+
+/**
+ *
+ * @author Owner
+ */
+//reference
+//coded and developed from psuedo code and writing in https://algorithmtutor.com/Data-Structures/Tree/Red-Black-Trees/
+public class BalancedPersistentDynamicSet<E extends Comparable<E>> extends BinarySearchTreeGivenTemplate {
+
+    public static final boolean RED = true;
+    public static final boolean BLACK = false;
+    public BinarySearchTreeGivenTemplate.BinaryTreeNode root;
+
+    //empty constructor for now
+    public BalancedPersistentDynamicSet() {
+
+    }
+
+}
